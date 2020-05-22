307e7224dec9c772fec02ac7509b4ce55408be4d
Sherif Amr <sherif.amr.927@gmail.com>
2020-05-22 18:31:06 +0200

    Update jwt.strategy.ts

--- src/auth/services/jwt.strategy.ts
+++ src/auth/services/jwt.strategy.ts
@@ -11,7 +11,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.SECRET || 'qwerty123',
+      secretOrKey: process.env.SECRET,
     });
   }
 
